a3614b814d828124bb92841241a5e400f5244508
kevvj <kevinjvillalba774+1@gmail.com>
2024-11-13 01:38:43 -0500

    first commitadqwdwqd

--- index.js
+++ index.js
@@ -14,14 +14,11 @@ app.get('/', (req, res) => {
     res.send('¡Bienvenido a la Api de login y registro!');
 });
 
-// Ruta para registrar usuarios
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
-
     if (!username || !password) {
         return res.status(400).json({ message: 'Faltan datos en el registro.' });
     }
-
     const usuarioExistente = usuarios.find(user => user.username === username);
 
     if (usuarioExistente) {
@@ -29,32 +26,26 @@ app.post('/register', async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
     usuarios.push({ username, password: hashedPassword });
-
     res.status(201).json({ message: 'Usuario registrado exitosamente' });
 });
 
 // Ruta para login
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;  
-
     if (!username || !password) {
         return res.status(400).json({ message: 'Faltan datos en el login' });
     }
 
     const usuario = usuarios.find(user => user.username === username);
-
     if (!usuario) {
         return res.status(400).json({ message: 'Usuario no encontrado.' });
     }
 
     const esValida = await bcrypt.compare(password, usuario.password);
-
     if (!esValida) {
         return res.status(401).json({ message: 'Contraseña incorrecta' });
     }
-
     res.status(200).json({ message: 'Login exitoso.' });
 });
 
