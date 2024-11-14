130fcd55b5b882142b7554bdc195de3bc77e0154
kevvj <kevinjvillalba774+1@gmail.com>
2024-11-14 03:02:09 -0500

    many-things

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'clothingstore'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err);
+        return;
+    }
+    console.log('Conectado a la base de datos MySQL');
+});
 
 const app = express()
 
@@ -21,22 +37,42 @@ app.get('/', (req, res) => {
 });
 
 app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
+    const { username, password, firstName, lastName, email } = req.body;
+    if (!username || !password || !firstName || !lastName || !email) {
         return res.status(400).json({ message: 'Faltan datos en el registro.' });
     }
-    const usuarioExistente = usuarios.find(user => user.username === username);
 
-    if (usuarioExistente) {
-        return res.status(400).json({ message: 'El usuario ya existe' });
-    }
+    const query = 'SELECT * FROM cliente WHERE nombre_usuario = ?';
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    usuarios.push({ username, password: hashedPassword });
-    res.status(201).json({ message: 'Usuario registrado exitosamente' });
+    db.query(query, [username], async (err, result) => {
+        if (err) {
+            console.error('Error al ejecutar la consulta de selección:', err.message);
+            return res.status(500).json({ message: 'Error en la consulta de la base de datos.' });
+        }
+
+        if (result.length > 0) {
+            return res.status(400).json({ message: 'El usuario ya existe.' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const FullName = firstName + " " + lastName;
+        const DateToday = new Date().toISOString().split('T')[0];
+
+        const insertQuery = 'INSERT INTO cliente (nombre_usuario, contraseña, nombre, email, direccion, fecha_registro) VALUES (?, ?, ?, ?, ?, ?)';
+
+        db.query(insertQuery, [username, hashedPassword, FullName, email, null, DateToday], (err, result) => {
+            if (err) {
+                console.error('Error al ejecutar la consulta de inserción:', err.message);
+                return res.status(500).json({ message: 'Error al registrar el usuario', error: err.message });
+            }
+            res.status(201).json({ message: 'Usuario registrado exitosamente' });
+        });
+
+    });
 });
 
-// Ruta para login
+
+
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
